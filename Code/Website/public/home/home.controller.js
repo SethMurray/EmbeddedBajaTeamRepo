f592d88de570ae3ed4cfeec85beaf64835a9708a
Unknown <david.macdonald77@yahoo.com>
2018-04-07 00:37:32 -0500

    Figured out way to connect to DB only once.  Set up preliminary temperature chart

--- Code/Website/public/home/home.controller.js
+++ Code/Website/public/home/home.controller.js
@@ -7,20 +7,94 @@
     homeController.$inject = ['$http'];
     function homeController($http) {
         let vm = this;
+        vm.temperatures = [];
+        vm.labels = [];
+        vm.tempChart = null;
 
-        vm.getTemperatures = function () {
-            console.log('Getting Temp');
-            let success = function (response) {
-                console.log(response);
-                vm.temperatures = response.data;
+        let success = function (response) {
+            console.log(response);
+            response.data.forEach(function(row) {
+                vm.temperatures.push(row.fahrenheit);
+                vm.labels.push(new Date(row.timestamp));
+            });
+            let chartDiv = document.getElementById('tempChart').getContext('2d');
+            let config = {
+                type: 'line',
+                data: {
+                    labels: vm.labels,
+                    datasets: [{
+                        label: "Temperature",
+                        data: vm.temperatures,
+                        backgroundColor: '#000000',
+                        fill: false
+                    }],
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Transmission Temperature'
+                    },
+                    tooltips: {
+                        mode: 'nearest',
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            time: {
+                                parser: 'll HH:mm:ss',
+                                tooltipFormat: 'll HH:mm:ss'
+                            },
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Time'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Temperature'
+                            }
+                        }]
+                    }
+                }
             };
+            vm.tempChart = new Chart(chartDiv, config);
 
-            let error = function (response) {
-                console.log(response);
-            };
-
-            $http.get('/api/temperatures').then(success, error);
+            //Update graph every 2 seconds
+            vm.interval = setInterval(vm.updateTempChart, 2000);
         };
+
+        let error = function (response) {
+            console.log(response);
+        };
+
+        $http.get('/api/temperatures').then(success, error);
+
+        vm.updateTempChart = function () {
+            if (vm.tempChart != null) {
+                let success = function(response) {
+                    console.log(response);
+                    //Update chart
+                    if (response.data.length > 0) {
+                        vm.tempChart.data.datasets[0].data.push(response.data[0].fahrenheit);
+                        vm.tempChart.data.labels.push(new Date(response.data[0].timestamp));
+                        vm.tempChart.update();
+                    }
+                };
+
+                let error = function(resonse) {
+                    console.log(error);
+                };
+                $http.get('/api/lastTemp').then(success, error);
+            }
+        };
+
+        vm.stopInterval = function() {
+            clearInterval(vm.interval);
+        }
     }
 
 })();
--- Code/Website/routes/api.js
+++ Code/Website/routes/api.js
@@ -3,48 +3,27 @@ let router = express.Router();
 let pg = require('pg');
 
 let constants = require('../constants');
+let db = require('../db.js');
 
 router.get('/temperatures', (req, res) => {
-    let client = new pg.Client({
-        host: constants.DBHost,
-        user: constants.DBUser,
-        password: constants.DBPassword,
-        database: constants.DBName,
-        port: 5432,
-        ssl: false
-    });
-    client.connect((err) => {
+    db.query('SELECT * FROM temperature', (err, result) => {
         if (err) {
             res.json(err);
         }
-        client.query('SELECT * FROM temperature', (err, result) => {
-            if (err) {
-                res.json(err);
-            }
+        if (result) {
             res.json(result.rows);
-        });
+        }
     });
 });
 
 router.get('/lastTemp', (req, res) => {
-    let client = new pg.Client({
-        host: constants.DBHost,
-        user: constants.DBUser,
-        password: constants.DBPassword,
-        database: constants.DBName,
-        port: 5432,
-        ssl: false
-    });
-    client.connect((err) => {
+    db.query('SELECT * FROM temperature ORDER BY timestamp DESC LIMIT 1', (err, result) => {
         if (err) {
-            res.json(err);
+            return res.json(err);
         }
-        client.query('SELECT * FROM temperature ORDER BY timestamp DESC LIMIT 1', (err, result) => {
-            if (err) {
-                res.json(err);
-            }
+        if (result) {
             res.json(result.rows);
-        });
+        }
     });
 });
 
